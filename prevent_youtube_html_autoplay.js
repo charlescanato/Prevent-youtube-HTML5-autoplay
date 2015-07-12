94447b80a697e450e2e5633bafc4f380a081e8af
Charles R. Canato <charles@inmov.net>
2015-07-12 15:59:46 -0300

    @includes and currentTime
    [*] Changed @includes so HTTPS is not ignored
    [-] removed currentTime change, 'cause you might be reloading a playing video page and we don't wanna mess with that

--- prevent_youtube_html_autoplay.js
+++ prevent_youtube_html_autoplay.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name           Youtube HTML5 stop autoplay
+// @name           Youtube HTML5 Stop AutoPlay
 // @namespace      http://smiler.se/
-// @description    Prevent HTML5 videos on youtube from autoplaying
-// @include        http://youtube.com/watch?*
-// @include        http://*.youtube.com/watch?*
-// @include        http://youtube.com/watch#*
-// @include        http://*.youtube.com/watch#*
-// @include        http://*.youtube.com/embed/*
-// @include        http://youtube.com/embed/*
+// @description    Prevent HTML5 videos on YouTube from autoplaying
+// @include        *://youtube.com/watch?*
+// @include        *://*.youtube.com/watch?*
+// @include        *://youtube.com/watch#*
+// @include        *://*.youtube.com/watch#*
+// @include        *://*.youtube.com/embed/*
+// @include        *://youtube.com/embed/*
+// @grant          none
 // ==/UserScript==
 
 function onLoad() {
 	setTimeout(function(){
-    	video = document.getElementsByTagName('video')[0];
+    video = document.getElementsByTagName('video')[0];
 		if (video) {
 		    video.pause();
-		    video.currentTime = 0;
 		}
 	}, 300);
 }
